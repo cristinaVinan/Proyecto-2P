985000463f72d65941d56dbc736f8546855ec03b
AngelBoza-Ecu <angmboza@espol.edu.ec>
2023-08-31 17:25:11 -0500

    Create the App -- Angel

--- production_app.py
+++ production_app.py
@@ -6,5 +6,20 @@ from streamlit_option_menu import option_menu
 import plotly.express as px
 from PIL import Image
 from collections import namedtuple
-from math import acos, asin, cos, sin, atan, sqrt, radians, degrees
+
+# Icon
+icon =Image.open("Resources/Logo Petro.png")
+
+# App configuration
+st.set_page_config(page_title='PetroGraphix', page_icon=icon)
+
+# App's title
+st.title('PetroGraphix')
+
+st.write('----')
+
+# App's description
+st.markdown("""This app helps you visualise production curves by year, IPR curves, 
+perform calculations, among other things.
+""")
 
